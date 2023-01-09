848c14554e998ff821001861e83decd8d66a6439
Wilmer Jimenez <wilmer.jimenez@epn.edu.ec>
2023-01-09 10:53:53 -0500

    Menu

--- 01.1-Android/app/src/main/java/com/example/movcomwmjt/BEntrenador.kt
+++ 01.1-Android/app/src/main/java/com/example/movcomwmjt/BEntrenador.kt
@@ -4,5 +4,8 @@ class BEntrenador (
     var nombre:String,
     var descripcion:String
 ){
+    override fun toString() : String{
+        return "${nombre}-${descripcion}"
+    }
 
 }
--- 01.1-Android/app/src/main/java/com/example/movcomwmjt/BListView.kt
+++ 01.1-Android/app/src/main/java/com/example/movcomwmjt/BListView.kt
@@ -1,16 +1,23 @@
 package com.example.movcomwmjt
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
 
 class BListView : AppCompatActivity() {
 
     val arreglo: ArrayList<BEntrenador> = BBaseDatosMemoria.arregloBEntrenador
 
-
+    var idItemSeleccionado =0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blist_view)
@@ -32,7 +39,85 @@ class BListView : AppCompatActivity() {
             .setOnClickListener{
                 anadirEntrenador(adaptador)
             }
+
+        registerForContextMenu(listView)
+
+    }// fin de OnCreate
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ){
+        super.onCreateContextMenu(menu, v, menuInfo)
+        //LLenamos las opciones del menu
+        val inflater=menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        //Obtener el id del ArrayListSeleccionada
+        val info = menuInfo as AdapterView.AdapterContextMenuInfo
+        val id = info.position
+        idItemSeleccionado=id
+
     }
+
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.mi_editar->{
+                "${idItemSeleccionado}"
+                return true
+            }
+            R.id.mi_eliminar->{
+                abrirDialogo()
+                "${idItemSeleccionado}"
+                return true
+            }
+            else->super.onContextItemSelected(item)
+
+        }
+    }
+
+    fun abrirDialogo(){
+        val builder=AlertDialog.Builder(this)
+        builder.setTitle("Desea eliminar")
+        builder.setPositiveButton(
+            "Aceptar",
+            DialogInterface.OnClickListener{dialog,
+            which->
+                //al aceptar eliminar el registro
+            }
+
+        )
+        builder.setNegativeButton(
+            "Cancelar",
+            null
+        )
+
+        val opciones= resources.getStringArray(
+            R.array.string_array_opciones_dialogo)
+        val seleccionPrevia= booleanArrayOf(
+            true, //Lunes seleccionado
+            false, // Martes no seleccionado
+             false, //Miercole no seleccionado
+        )
+
+        builder.setMultiChoiceItems(
+            opciones,
+            seleccionPrevia,
+            {dialog,
+            which,
+            isChecked->
+                "Dio click en el item${which}"
+            }
+        )
+
+        val dialogo=builder.create()
+        dialogo.show()
+    }
+
+
+
+
 
     fun anadirEntrenador(adaptador: ArrayAdapter<BEntrenador>){
         arreglo.add(
